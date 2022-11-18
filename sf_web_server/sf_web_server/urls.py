aa72e69c899096595fe3cae28d0209f670ebd5a7
Dheeraj <dheerajkumar.kumar18@gmail.com>
2022-11-18 00:57:31 -0500

    urls django admin site changes

--- sf_web_server/sf_web_server/urls.py
+++ sf_web_server/sf_web_server/urls.py
@@ -6,6 +6,10 @@ from django.contrib import admin
 router = routers.DefaultRouter()
 router.register(r'groups', views.GroupViewSet)
 
+admin.site.site_header = "Silver Foxes Admin"
+admin.site.site_title = "Silver Foxes Admin Portal"
+admin.site.index_title = "Welcome to Silver Foxes Admin Portal"
+
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
