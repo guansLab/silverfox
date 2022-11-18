81e3da5b7818cd8a5d0726e73260a282a03eedca
Dheeraj <dheerajkumar.kumar18@gmail.com>
2022-11-18 00:49:37 -0500

    Content Heirarchy Changes

--- sf_web_server/sf_web_server/urls.py
+++ sf_web_server/sf_web_server/urls.py
@@ -1,31 +1,16 @@
-"""sf_web_server URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.urls import include, path
+from rest_framework import routers
+from sf_web_server.user_auth import views
 from django.contrib import admin
-from django.urls import path
 
-admin.site.site_header = "Silver Foxes Admin"
-admin.site.site_title = "Silver Foxes Admin Portal"
-admin.site.index_title = "Welcome to Silver Foxes Admin Portal"
-
-admin.site.site_header = "Silver Foxes Admin"
-admin.site.site_title = "Silver Foxes Admin Portal"
-admin.site.index_title = "Welcome to Silver Foxes Admin Portal"
+router = routers.DefaultRouter()
+router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('', include(router.urls)),
 ]
