199c54d737be5e0b69470c073e377c86baa857eb
Dheeraj <dheerajkumar.kumar18@gmail.com>
2022-11-29 16:00:00 -0500

    static and media root

--- sf_web_server/sf_web_server/settings.py
+++ sf_web_server/sf_web_server/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = 'django-insecure-7nq5*&r3badmo(4r9jezck=h9*#u=w&4sa)@+g=9#@#*8w%$2r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = './static'
+MEDIA_ROOT = './media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
