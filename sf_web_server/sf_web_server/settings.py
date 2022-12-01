32aeb708cb636a65fe60784b320cb6c9bda5d31c
Dheeraj <dheerajkumar.kumar18@gmail.com>
2022-12-01 16:25:56 -0500

    added yarn and linked django and front-end

--- sf_web_server/sf_web_server/settings.py
+++ sf_web_server/sf_web_server/settings.py
@@ -6,7 +6,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import pathlib
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'sf_web_server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [pathlib.Path(BASE_DIR).parent.joinpath("sf_web_client")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,10 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = './static'
 MEDIA_ROOT = './media'
+
+STATICFILES_DIRS = [
+    pathlib.Path(BASE_DIR).parent.joinpath("sf_web_client", 'build', 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- sf_web_server/sf_web_server/urls.py
+++ sf_web_server/sf_web_server/urls.py
@@ -1,13 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
-from sf_web_server.user_auth.views import GroupViewSet, LoginView, RegisterView, LogoutView
+from sf_web_server.user_auth.views import GroupViewSet, LoginView, RegisterView, LogoutView, index_view
 from sf_web_server.content.views import ContentCategoryViewSet, ContentViewSet
 from django.contrib import admin
 
 router = routers.DefaultRouter()
-router.register(r'groups', GroupViewSet)
-router.register(r'content-category', ContentCategoryViewSet)
-router.register(r'content', ContentViewSet)
+router.register(r'^groups$', GroupViewSet)
+router.register(r'^content-category$', ContentCategoryViewSet)
+router.register(r'^content$', ContentViewSet)
 
 admin.site.site_header = "Silver Foxes Admin"
 admin.site.site_title = "Silver Foxes Admin Portal"
@@ -16,9 +16,10 @@ admin.site.index_title = "Welcome to Silver Foxes Admin Portal"
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('', index_view, name='index'),
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('', include(router.urls)),
+    path(r'app/', include(router.urls))
 ]
