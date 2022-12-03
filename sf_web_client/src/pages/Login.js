1e11e062e309048e4567e53179683cad324563ac
Dheeraj <dheerajkumar.kumar18@gmail.com>
2022-12-03 00:01:40 -0500

    signup and login

--- sf_web_client/src/pages/Login.js
+++ sf_web_client/src/pages/Login.js
@@ -1,21 +1,67 @@
 import './Login.css';
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from '../axios';
+
+const LOGIN_URL = '/login/';
+
+
+const loginSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid Email address").required("Required"),
+  password: Yup.string().min(8, "Must be 8 characters or more").required("Required"),
+});
 
 function Login() {
-  return (
-    <div id="loginBox">
-      <form className="form" onSubmit={console.log("submitted")}>
-          <div className="input-group">
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" placeholder="name@email.com" />
+  const handleSubmit = async (values) => {
+    try{
+      const response = await axios.post(LOGIN_URL,{
+          username: values.email, 
+          password: values.password,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          withCredentials: false
+        }
+      );
+      console.log(response.data);
+    }
+    catch(err){
+        console.log(JSON.stringify(err));
+        console.log("Registration Failed");
+    }
+    console.log(values);
+  };
+
+    return (
+      <Formik
+      initialValues={{email: '', password: ''}}
+      validationSchema={loginSchema}
+      onSubmit={handleSubmit}>
+        <Form id="loginBox">
+          <div className="form">
+            <div className="input-group">
+                <label>
+                  Email: <Field type="email" name="email"  className="input-field" placeholder="sample@email.com"/>
+                <ErrorMessage name="email" component="div" />
+              </label>
+            </div>
+            <div className="input-group">
+              <label>
+                Password:
+                <Field type="password" name="password"  className="input-field"/>
+                <ErrorMessage name="password" component="div" />
+              </label>
+            </div>
+            <button type="submit" className="primary">
+              Submit
+            </button>
           </div>
-          <div className="input-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" />
-          </div>
-          <button className="primary">Enter</button>
-        </form>
-    </div>
-  );
-}
+        </Form>
+      </Formik>
+    );
+  } 
 
 export default Login;
--- sf_web_client/src/pages/Signup.js
+++ sf_web_client/src/pages/Signup.js
@@ -1,29 +1,97 @@
+import React from "react";
 import './Signup.css';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from '../axios';
+
+
+const REGISTER_URL = '/register/';
+
+
+const signupSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid Email address").required("Required"),
+  firstName: Yup.string().required("Required"),
+  lastName: Yup.string().required("Required"),
+  password: Yup.string().min(8, "Must be 8 characters or more").required("Required"),
+  confirmPassword: Yup.string().test('passwords-match', 'Passwords must match', function(value){
+    return this.parent.password === value;
+  })
+});
+
 
 function Signup() {
-  return (
-    <div id="signupBox">
-      <form className="form" onSubmit={console.log("submitted")}>
-      <div className="input-group">
-            <label htmlFor="name">Full Name</label>
-            <input type="text" name="name" placeholder="John Doe"/>
+
+  const handleSubmit = async (values) => {
+    try{
+      const response = await axios.post(REGISTER_URL,{
+          email: values.email, 
+          first_name: values.firstName,
+          last_name: values.lastName,
+          password: values.password,
+          confirm_password: values.confirmPassword
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          withCredentials: false
+        }
+      );
+      console.log(response.data);
+    }
+    catch(err){
+        console.log(JSON.stringify(err));
+        console.log("Registration Failed");
+    }
+    console.log(values);
+  };
+
+    return (
+      <Formik
+      initialValues={{email: '', password: '', firstName: '', lastName: '', confirmPassword: ""}}
+      validationSchema={signupSchema}
+      onSubmit={handleSubmit}>
+        <Form id="signupBox">
+          <div className="form">
+            <div className="input-group">
+              <label>
+                First Name: <Field type="text" name="firstName" className="input-field" placeholder="John"/>
+                <ErrorMessage name="firstName" component="div" />
+              </label>
+            </div> 
+            <div className="input-group">
+              <label>
+                Last Name: <Field type="text" name="lastName"  className="input-field" placeholder="Doe"/>
+                <ErrorMessage name="lastName" component="div" />
+              </label>
+            </div>
+            <div className="input-group">
+                <label>
+                  Email: <Field type="email" name="email"  className="input-field" placeholder="sample@email.com"/>
+                <ErrorMessage name="email" component="div" />
+              </label>
+            </div>
+            <div className="input-group">
+              <label>
+                Password:
+                <Field type="password" name="password"  className="input-field"/>
+                <ErrorMessage name="password" component="div" />
+              </label>
+            </div>
+            <div className="input-group">
+              <label>
+                Confirm Password:
+                <Field type="password" name="confirmPassword"  className="input-field"/>
+                <ErrorMessage name="confirmPassword" component="div" />
+              </label>
+            </div>
+            <button type="submit" className="primary">
+              Submit
+            </button>
           </div>
-          <div className="input-group">
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" placeholder="johndoe@email.com" />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" />
-          </div>
-          <div className="input-group">
-            <label htmlFor="confirmPassword">Confirm Password</label>
-            <input type="password" name="password" />
-          </div>
-          <button className="primary">Enter</button>
-        </form>
-    </div>
-  );
-}
+        </Form>
+      </Formik>
+    );
+  } 
 
 export default Signup;
