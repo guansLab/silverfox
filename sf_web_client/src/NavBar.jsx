44c2bdd1635359a4552b8bf0d95433d38fbcaa61
Taksch A. Dube <takschdube@takschdube.com>
2022-12-05 17:40:47 -0500

    logout switch is in, not sure if correct

--- sf_web_client/src/NavBar.jsx
+++ sf_web_client/src/NavBar.jsx
@@ -1,6 +1,38 @@
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Navbar() {
+  const { isLoggedIn } = useSelector(state => state.auth);
+
+  if (isLoggedIn) {
+    return(
+    <nav style=
+    {{
+      position: "relative", 
+      width: "100%", 
+      height: "20%",
+      fontSize: "Larger"}}>
+      <ul style=
+      {{
+        listStyleType: "none",
+        margin: "0",
+        padding: "10px",
+      }}> 
+        <li style={{display: "inline", padding: "10px", paddingRight: "55%"}}>
+          <img src="../logoCopy.png" alt="Kent Silver Foxes" />
+        </li>
+        <li style={{display: "inline", padding: "10px"}}>
+          <Link to="/">Home</Link>
+        </li>
+        <li style={{display: "inline", padding: "10px"}}>
+          <Link to="/">Logout</Link> {/* This should work, but page goes blank*/}
+        </li>
+      </ul>
+    </nav>
+    
+  );
+  }
+
   return (
     <nav style=
     {{
@@ -13,7 +45,7 @@ function Navbar() {
         listStyleType: "none",
         margin: "0",
         padding: "10px",
-      }}>
+      }}> 
         <li style={{display: "inline", padding: "10px", paddingRight: "55%"}}>
           <img src="../logoCopy.png" alt="Kent Silver Foxes" />
         </li>
