3b9169b7be35cf6a8f99ddb193727e2af4b678d4
Dheeraj <dheerajkumar.kumar18@gmail.com>
2022-12-16 14:47:46 -0500

    minor changes

--- sf_web_server/sf_web_server/settings.py
+++ sf_web_server/sf_web_server/settings.py
@@ -148,32 +148,3 @@ REST_FRAMEWORK = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ORIGIN_WHITELIST = (
-#   'http://localhost:3000',
-#   'http://localhost:8000',
-# )
-#
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-# ]
-#
-# CORS_ALLOW_METHODS = [
-# 'DELETE',
-# 'GET',
-# 'OPTIONS',
-# 'PATCH',
-# 'POST',
-# 'PUT',
-# ]
-#
-# CORS_ALLOW_HEADERS = [
-# 'accept',
-# 'accept-encoding',
-# 'authorization',
-# 'content-type',
-# 'dnt',
-# 'origin',
-# 'user-agent',
-# 'x-csrftoken',
-# 'x-requested-with',
-# ]
