ef492e997671283ec3bd21129f3bbc4cc4e0a4ff
Dheeraj <dheerajkumar.kumar18@gmail.com>
2022-12-17 15:30:19 -0500

    Nav bar login logout styling fix

--- sf_web_client/src/NavBar.jsx
+++ sf_web_client/src/NavBar.jsx
@@ -6,42 +6,6 @@ function Navbar() {
   const { username } = useSelector(state => state.session);
   const nav = useNavigate();
 
-  if (username) {
-    return(
-    <nav style=
-    {{
-      position: "relative", 
-      width: "100%", 
-      height: "20%",
-      fontSize: "Larger"}}>
-      <ul style=
-      {{
-        listStyleType: "none",
-        margin: "0",
-        padding: "10px",
-      }}> 
-        <li style={{display: "inline", padding: "1%", paddingRight: "55%"}}>
-          <img className={"logo"} src="../logoCopy.png" alt="Kent Silver Foxes" onClick={ () => {nav("/") }}/>
-          <ul style={{display: "inline", padding: "1%", float:"right"}}>
-            <li style={{display: "inline", padding: "10px"}}>
-              <Link to="/logout">Logout</Link>
-            </li>
-          </ul>
-          <ul style={{display: "inline", float:"right"}}>
-            <li style={{display: "inline", padding: "10px"}}>
-              <Link to="/about">About Us</Link>
-            </li>
-            <li style={{display: "inline", padding: "10px"}}>
-              <Link to="/contact">Contact Us</Link>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </nav>
-    
-  );
-  }
-
   return (
     <nav style=
     {{
@@ -59,10 +23,10 @@ function Navbar() {
           <img className={"logo"} style={{ hover: "pointer" }} src="../logoCopy.png" alt="Kent Silver Foxes" onClick={ () => {nav("/") }}/>
           <ul style={{display: "inline", padding: "1%", float:"right"}}>
             <li style={{display: "inline", padding: "10px"}}>
-              <Link to="/login">Login</Link>
+              {username?<Link to="/logout">Logout</Link>:<Link to="/login">Login</Link>}
             </li>
             <li style={{display: "inline", padding: "10px"}}>
-              <Link to="/signup">Sign Up</Link>
+              {username?<></>:<Link to="/signup">Sign Up</Link>}
             </li>
           </ul>
           <ul style={{display: "inline", float:"right"}}>
